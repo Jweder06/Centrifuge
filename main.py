cc52ccb04bc7a58442f9ae6f3ca60ca6d003ffd1
Jweder06 <jweder06@charlottesvilleschools.org>
2023-05-23 09:57:18 -0400

    code

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from time import sleep
-
-import digitalio as DIO
-
-coils =[
-    DIo.DigitalInOut(board.D8),  # A1
-    DIo.DigitalInOut(board.D9),  # A2
-    DIo.DigitalInOut(board.D10),  # B1
-    DIo.DigitalInOut(board.D11),  # B2
-    ]
-for coil in coils:
-    coil.direction = DIo.Direction.OUTPUT
-
-motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)
-
-
-
+import time
+import board
+from digitalio import DigitalInOut, Direction, Pull
+from pwmio import PWMOut
+from adafruit_motor import motor as Motor
+Bvalue = False
+drv8833_ain1 = PWMOut(board.D8, frequency=50)
+drv8833_ain2 = PWMOut(board.D9, frequency=50)
+drv8833_bin1 = PWMOut(board.D12, frequency=50)
+drv8833_bin2 = PWMOut(board.D11, frequency=50)
+button_a = DigitalInOut(board.D7)
+drv8833_sleep = DigitalInOut(board.D5)
+motor_a = Motor.DCMotor(drv8833_ain1, drv8833_ain2)
+button_a.direction = Direction.INPUT
+button_a.pull = Pull.UP
+drv8833_sleep.direction = Direction.OUTPUT
+drv8833_sleep.value = True  # enable (turn on) the motor driver
+buttonstate="not pressed"
+print("helllo world")
 while True:
-    print("Hello world!")
-    sleep(1)
+    print(Bvalue)
+    if button_a.value == 0 and buttonstate == "not pressed": 
+        Bvalue = not Bvalue
+        buttonstate = "pressed"
+    elif button_a.value == 1: 
+        buttonstate = "not pressed"
+    if Bvalue == True:
+            motor_a.throttle = 1
+    elif         
+
+   
